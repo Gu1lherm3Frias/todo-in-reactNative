13d2e33b20dd25e7d6b561c11f1175a42d79dd74
Guilherme Frias <guilherme.frias.o.t3@gmail.com>
2022-07-22 22:23:27 -0300

    [bug] - gestureHandler bug

---
+++ src/screens/main-screen.tsx
@@ -0,0 +1,30 @@
+import React, { useCallback, useState } from 'react'
+import { Pressable } from 'react-native'
+import { Text, Box, Center, VStack, useColorModeValue } from 'native-base'
+import ThemeToggle from '../components/theme-toggle'
+import AnimatedCheckbox from '../components/animated-checkbox'
+import TaskItem from '../components/task-item'
+
+export default function MainScreen() {
+  const  [ checked, setChecked ] = useState(false)
+  
+  const handlePressCheckbox  = useCallback(() => {
+    setChecked(prev => !prev)
+  }, [])
+
+  return (
+    <Center 
+      _dark={{bg: 'blueGray.900'}} 
+      _light={{bg: 'blueGray.50'}} 
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <Box w="200px" h="50px">
+          <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckbox} />
+        </Box>
+        <ThemeToggle />
+      </VStack>
+    </Center>
+  )
+}
